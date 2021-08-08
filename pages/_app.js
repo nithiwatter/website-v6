c331e7b66947ffb5ad0275ce5002910d0e5048a5
Calix Huang <calix.huang1@gmail.com>
2021-08-07 18:54:18 -0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../styles/main.scss";
+import "tailwindcss/tailwind.css";
 
-export default MyApp
+const _app = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default _app;
