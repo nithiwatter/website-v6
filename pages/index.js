208c408fb5061a41255dc792032f621a5078b337
Calix Huang <calix.huang1@gmail.com>
2022-05-18 17:42:27 -0700

    meta

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import Intro from "../components/Home/Intro";
 import Experience from "../components/Home/Experience";
@@ -7,6 +8,10 @@ import Contact from "../components/Home/Contact";
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>Calix Huang • @calixo888</title>
+        <meta name="description" content="Hey, I'm Calix. I love software and storytelling. I'm an incoming freshman at the USC Iovine and Young Academy. Learn more about me here :)" />
+      </Head>
       <Intro/>
       <Experience/>
       <Journey/>
